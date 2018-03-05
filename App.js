d4da7e54d907bfe256ff4320df05811dcd4e4e1c
Rahul Verma <rahulverma@ccs.neu.edu>
2018-03-04 21:33:26 -0500

    Initial Steps for creating a demo React-Native-Application

---
+++ App.js
@@ -0,0 +1,22 @@
+/**
+ * @author: Rahul Verma, Vrushal KulKarni
+ * @Version 0.0.1
+ */
+
+import React, { Component } from 'react';
+import {
+  Platform,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+import Main from './Components/main'
+
+
+export default class App extends React.Component {
+    render() {
+        return (
+            <Main />
+        );
+    }
+}
